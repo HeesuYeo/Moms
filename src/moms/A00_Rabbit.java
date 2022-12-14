7de55c1b45cc1f55ff7e22c56a035f4a575efeb3
Gi7C-09 <Gi7C-09@Gi7C-11>
2022-12-14 15:11:47 +0900

    test

---
+++ src/moms/A00_Rabbit.java
@@ -0,0 +1,16 @@
+package moms;
+
+public class A00_Rabbit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("=====================");
+		System.out.println("         /)/)        ");
+		System.out.println("        ( ..)        ");
+		System.out.println("        ( >$)      	 ");
+		System.out.println("  Have a Good Time.  ");
+		System.out.println("=====================");
+		// 미관상 했지만 끝에 띄어쓰기는 의미가 x
+	}
+
+}
